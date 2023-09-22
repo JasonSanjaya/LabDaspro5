861e2fb4fbec286dd5052b2089e44aeae05bbba7
Jasonsanjaya <jasonsanjaya1503@gmail.com>
2023-09-22 09:41:22 +0700

    kuis 1

---
+++ biodata_pribadi.pas
@@ -0,0 +1,13 @@
+program biodata_pribadi;
+var nama,NIM,kom,alamat:string;
+begin 
+write('Masukan Nama Lengkap Anda: ');
+readln(nama);
+write('Masukan NIM Anda: ');
+readln(NIM);
+write('Masukan Kom Anda: ');
+readln(kom);
+Write('Masukan Alamat Anda: ');
+readln(alamat);
+writeln('Hallo, nama saya ',nama,', NIM saya ',NIM,', saya di kom ',kom,', alamat saya di ',alamat,'. Salam Kenal Semua!')
+end.
