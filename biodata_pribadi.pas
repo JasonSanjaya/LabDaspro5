0b5d4037d3129b55d671cd5c521df0a2b071ea7a
JasonSanjaya <145695216+JasonSanjaya@users.noreply.github.com>
2023-09-24 12:38:59 +0700

    biodata_pribadi.pas

--- biodata_pribadi.pas
+++ biodata_pribadi.pas
@@ -1,13 +1,19 @@
-program biodata_pribadi;
-var nama,NIM,kom,alamat:string;
-begin 
-write('Masukan Nama Lengkap Anda: ');
-readln(nama);
-write('Masukan NIM Anda: ');
-readln(NIM);
-write('Masukan Kom Anda: ');
-readln(kom);
-Write('Masukan Alamat Anda: ');
-readln(alamat);
-writeln('Hallo, nama saya ',nama,', NIM saya ',NIM,', saya di kom ',kom,', alamat saya di ',alamat,'. Salam Kenal Semua!')
+Program Biodata_pribadi;
+uses crt;
+var 
+nama,alamat:string;
+NIM:longint;
+Kom:char;
+
+begin
+clrscr;
+ write('Masukan Nama Lengkap Anda: ');
+ readln(nama);
+ write('Masukan NIM Anda: ');
+ readln(NIM);
+ write('Masukan Kom Anda: ');
+ readln(kom);
+ write('Masukan Alamat Anda: ');
+ readln(alamat);
+ writeln('Hallo, nama saya ',nama,', NIM saya ',NIM,', saya di Kom ',kom,', alamat saya di ',alamat,'.',' Salam Kenal Semua!')
 end.
